fc5c31509f40d3da2a91ad8f4c561d5602f8cab0
eniekeine <eniekeine@gmail.com>
2023-12-28 03:37:38 +0900

    change example url

--- src/index.js
+++ src/index.js
@@ -31,7 +31,7 @@ function onSelectionChanged(graph, domItem) {
 
 document.addEventListener('DOMContentLoaded', () => {
   const frame = document.querySelector('.frame');
-  fetch('/example/basic.json')
+  fetch('https://github.com/eniekeine/nodegraph/blob/master/example/basic.json')
     .then((response) => response.json())
     .then((graph) => {
       // initFrame must be called with frame element before any other calls
